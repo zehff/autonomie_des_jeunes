b00bb3d0f31ffda9cbf172e060b84069ede48ec6
zehff <azerty544@hotmail.com>
2014-02-14 12:11:20 +0100

    Fin du cours du 14/02/2014 autonomie des jeunes
    Début de codage des trois ménages

--- tests/example.py
+++ tests/example.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from datetime import datetime
+import datetime
 
 from openfisca_core import model
 import openfisca_france
@@ -12,17 +12,63 @@ def case_study (year = 2013) :
 #simulation est une classe , Scenariosimulation  est une instance, on a instancier "simulation"
 #on a mainteant fait simulation.set_congif pour modifier les attributs. on lui a dit de prendre l'année 2013
 #avec year=year. reforme = true. Nmen est le nombre de ménage. Maxrev est le salaire imposable de la première personne déclaré.
-# Ici tu prends que deux menage et on fait varier les salaire. Simulation.set.param est la legislation par défaut.
+# Ici tu prends que deux menage et on fait varier les salaires. Simulation.set.param est la legislation par défaut.
 
     simulation = ScenarioSimulation()
     simulation.set_config(year = year,
                           nmen = 2,
                           maxrev = 10000,
                           x_axis= 'sali')
+
+    simulation.set_param()
+    df = simulation.get_results_dataframe()
+    print df.to_string()
+  
+#Projet ménange enfant : Arbitrage entre garder l'enfant pour avoir le quotient familial ou perdre la demi part
+# en donnant une pension. 2 fonctions à tester : un ménage avec un enfant et un sans enfants.
+#pour rajouter des personnes simulation.scenario.add
+#Dans openfisca-france data.py variables de d'entré et model.py variables de sortie
+
+def menage_avec_enfant(year = 2013):
+    simulation = ScenarioSimulation()
+    simulation.set_config(year = year,
+                          nmen = 1)    
+    simulation.scenario.addIndiv(1, datetime.date(1975,1,1), 'conj','part')
+    simulation.scenario.addIndiv(2, datetime.date(1990,1,1), 'pac', 'enf')
+    
+    print simulation.scenario
+    print simulation.scenario.indiv[0]
+    
+    simulation.scenario.indiv[0]['sali'] = 30000
+    
+    
     simulation.set_param()
     df = simulation.get_results_dataframe()
     print df.to_string()
     
+def menage_sans_enfant(year = 2013):
+    simulation = ScenarioSimulation()
+    simulation.set_config(year = year,
+                          nmen = 10,
+                          maxrev = 100000,
+                          x_axis= 'sali')
+    print simulation.scenario
+    
+    
+    simulation.scenario.addIndiv(1, datetime.date(1975,1,1), 'conj','part')
+    
+def menage_enfant_seulement(year = 2013):
+    simulation = ScenarioSimulation()
+    simulation.set_config(year = year,
+                          nmen = 10,
+                          maxrev = 0,
+                          x_axis= 'sali')
+    print simulation.scenario
+    
+    
+    
 
 if __name__ == '__main__' :
-    case_study()
+    menage_avec_enfant()
+    
+    
